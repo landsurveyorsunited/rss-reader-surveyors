5cc5978de9847893e03f38897c43c0f85d274ecb
Justin Farrow <landsurveyorsunited@gmail.com>
2024-04-23 16:59:51 -0400

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,14 +7,14 @@ export const Header = () => {
     <header className='header'>
       <nav className="navbar sticky-top navbar-expand-lg navbar-dark bg-primary">
         <div className="container">
-          <NavLink to="/" className="navbar-brand">RSS Tennis Feed</NavLink>
+          <NavLink to="/" className="navbar-brand">Land Surveyor News</NavLink>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarRSSFeed" aria-controls="navbarRSSFeed" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
           <div className="collapse navbar-collapse justify-content-between align-items-center buttons-container" id="navbarRSSFeed">
             <div className="navbar-nav">
-              <NavLink to="/news-feed" className={({isActive}) => ('nav-link ' + (isActive ? "active" : ''))}>News</NavLink>
-              <NavLink to="/photos-feed" className={({isActive}) => ('nav-link ' + (isActive ? "active" : ''))}>Image Gallery</NavLink>
+              <NavLink to="/news-feed" className={({isActive}) => ('nav-link ' + (isActive ? "active" : ''))}>Surveying News</NavLink>
+              <NavLink to="/photos-feed" className={({isActive}) => ('nav-link ' + (isActive ? "active" : ''))}>Surveyor Photos</NavLink>
               <NavLink to="/about" className={({isActive}) => ('nav-link ' + (isActive ? "active" : ''))}>About</NavLink>
             </div>
             <div className='navbar-nav'>
